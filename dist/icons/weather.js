05be23a7d97b909b58ca79c25de3b639a482d8d4
photino <panzan89@gmail.com>
2015-08-01 15:38:05 +0800

    Add docs

---
+++ dist/icons/weather.js
@@ -0,0 +1,13 @@
+/**
+ * Weather Icons (Built with IcoMoon)
+ *  - Font Awesome: Dave Gandy, GPL / CC BY 4.0
+ */
+
+(function ($) {
+  'use strict';
+
+  schema.icons = $.extend(schema.icons, {
+
+  });
+
+})(jQuery);
